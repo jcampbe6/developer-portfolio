8f5f30e68e10e42b945e471dda3b8b7819ade55c
jcampbe6 <jcampbe6@ggc.edu>
2015-11-21 15:14:36 -0500

    jQuery animation to social media links, fixed glyphicon path issue, edited project thumbnails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,25 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery-2.1.4.min
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
 //= require_tree .
-//= require jquery-2.1.4.min
+
 //= require bootstrap.min
+
+$(document).ready(function(){
+
+    //grow social media links in footer on hover, then shrink back on mouse leave
+    $('.social-media-link').hover(
+        function(){
+            $(this).removeClass("icon-3x");
+            $(this).addClass("icon-4x")},
+        function(){
+            $(this).removeClass("icon-4x");
+            $(this).addClass("icon-3x")
+    });
+
+});
